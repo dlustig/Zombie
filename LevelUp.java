e396d42539bc5d7e6bf9c1c7643019a20588c029
ensign-redshirt <alewis3@mail.umw.edu>
2013-12-02 17:34:47 -0500

    Create LevelUp.java
    Basic code. Haven't done more than this yet.

---
+++ LevelUp.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class LevelUp{
+	public LevelUp(){
+	//create and set up the window
+	JFrame level = new JFrame("High Scores");
+
+	//make the program close when the window closes
+	level.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	level.getContentPane().setLayout(null);
+
+	}
+}
