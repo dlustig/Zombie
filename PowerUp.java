b3f7e5cba25e2e0f9e384c75703446092b2fc881
ensign-redshirt <alewis3@mail.umw.edu>
2013-12-02 17:34:26 -0500

    Create PowerUp.java
    Basic code. Haven't done more than this yet.

---
+++ PowerUp.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class PowerUp{
+	public PowerUp(){
+	//create and set up the window
+	JFrame power = new JFrame("High Scores");
+
+	//make the program close when the window closes
+	power.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	power.getContentPane().setLayout(null);
+
+	}
+}
