931c6993c9f5fa4e12ed58a9bd99839f780f0636
ensign-redshirt <alewis3@mail.umw.edu>
2013-12-03 23:05:21 -0500

    Update LevelUp.java

--- LevelUp.java
+++ LevelUp.java
@@ -1,13 +1,9 @@
-import javax.swing.*;
+
 
 public class LevelUp{
-	public LevelUp(){
-	//create and set up the window
-	JFrame level = new JFrame("High Scores");
-
-	//make the program close when the window closes
-	level.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	level.getContentPane().setLayout(null);
-
+	public LevelUp(int EXP, int lvl){
+		// how are we doing levels and points? 
+		// what are the numbers? 
+		// where do we need to keep track of this data?
 	}
 }
