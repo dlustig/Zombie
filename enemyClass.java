3737774002cf57286cc40191b12061b3ef0a13be
Csknight <dlustig@mail.umw.edu>
2013-12-09 16:20:10 -0500

    Update enemyClass.java

--- enemyClass.java
+++ enemyClass.java
@@ -354,20 +354,7 @@ class GameWorld extends JComponent implements KeyListener {
 
 
 public class enemyClass {
-	public static void main(String args[]) {
-		// create and set up the window.
-		JFrame frame = new JFrame("EnemyFactory!");
-
-		// make the program close when the window closes
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// add the GameWorld component
-		GameWorld g = new GameWorld( );
-		frame.add(g);
-		frame.addKeyListener(g);
-
-		// display the window.
-		frame.setSize(500, 500);
-		frame.setVisible(true);
-	}    
+	public enemyClass(){
+		GameWorld = new GameWorld();
+	}
 }
